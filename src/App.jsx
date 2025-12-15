34b030e506ae5a9140d745ec1ba9656ec36b99ff
flam3yx <154548576+flam3yx@users.noreply.github.com>
2025-12-15 01:24:17 +0100

    feat: add main sections for portfolio including About, Contact, Footer, Hero, Navbar, Projects, and Skills with responsive design and toast notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Home } from "./pages/home"
+import { Home } from "./pages/Home"
 import { NotFound } from "./pages/NotFound"
+import { Toaster } from "@/components/ui/toaster"
 
 function App() {
   return (
     <>
+      <Toaster />
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />}/> //* not found page
+          <Route path="*" element={<NotFound />} /> //* not found page
         </Routes>
       </BrowserRouter>
     </>
